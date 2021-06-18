3bef9795bdd6faa83cd09c2c240619b14a587887
Vanessa Velasco-Valles <2023vvelasco@arisehighschool.org>
2021-06-18 11:13:43 -0700

    i made 5 phrases

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
 var qwerty = document.getElementById('qwerty');
 var phrase = document.getElementById('phrase');
 var missed = 0;
+
+btn__reset.addEventLinstener('click')
+
+const phrases = [ 'drawing is something i enjoy', 'its really hot today', 'i want to watch anime', 'im going to get ice cream', 'lets go somewhere fun' ];
