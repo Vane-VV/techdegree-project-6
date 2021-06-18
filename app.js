98d4abaad8342f09be4e083dfb39c03b0751bd74
Vanessa Velasco-Valles <2023vvelasco@arisehighschool.org>
2021-06-18 12:57:22 -0700

    hid the start screen overlay

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
-var qwerty = document.getElementById('qwerty');
-var phrase = document.getElementById('phrase');
-var missed = 0;
+const qwerty = document.getElementById('qwerty');
+const phrase = document.getElementById('phrase');
+const overlay = document.getElementById('overlay');
 
-btn__reset.addEventLinstener('click')
+let missed = 0;
 
-const phrases = [ 'drawing is something i enjoy', 'its really hot today', 'i want to watch anime', 'im going to get ice cream', 'lets go somewhere fun' ];
+const phrases = [ 
+    'drawing is something i enjoy',
+    'its really hot today',
+    'i want to watch anime',
+    'im going to get ice cream',
+    'lets go somewhere fun' ];
+
+overlay.addEventListener('click', (e) => {
+   if (e.target.className === "btn__reset") {
+       overlay.style.display = 'none';
+   }
+});
