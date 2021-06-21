e9195da3d4015c9414cea82db935d368ea2039ee
Vanessa Velasco-Valles <2023vvelasco@arisehighschool.org>
2021-06-21 10:45:28 -0700

    added another function

--- app.js
+++ app.js
@@ -16,3 +16,23 @@ overlay.addEventListener('click', (e) => {
        overlay.style.display = 'none';
    }
 });
+
+
+// takes in an array of strings and returns an array containing the characters of a random string from the original array
+function getRandomPhraseAsArray(array) {
+    const number = Math.floor(Math.random() * array.length); // some integer between 0 and array.length - 1
+    const phrase = array[number]; 
+    const result = phrase.split('');
+    return result; //array containing characters, i.e. ['h', 'e', 'l', 'l', 'o']
+}
+
+getRandomPhraseAsArray(phrases)
+
+function addPhraseToDisplay(array) {
+    for (let i = 0; i < phrases.length; i ++){
+        let ul = document.getElementsByTagName('ul')
+        let li = document.createElement('li')
+        list.textContent = array[i]
+        list.appendChild(ul)
+    }
+}
